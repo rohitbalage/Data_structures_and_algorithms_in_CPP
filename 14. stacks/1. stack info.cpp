b24b27ffff8f28e1e2150ba9ca294ef17be84461
Rohit Balage <thatcomputerboy26@gmail.com>
2026-09-08 18:57:38 -0400

    added stack data structure

---
+++ 14. stacks/1. stack info.cpp
@@ -0,0 +1,20 @@
+/*
+
+Stack - like a bucket!  
+
+Top O(1)  - seeing top element
+
+Pop O(1) - remove top most element
+
+Push O(1) - push the data in to stack
+
+In real life - we see this in book rack example, when you remove book - you remove topmost book
+you canot remove the top bottom book.
+
+in Computer -
+
+H E L L T Y    ;  Y was entered wrongly - hence when we undo it removed from stack.
+
+
+
+*/

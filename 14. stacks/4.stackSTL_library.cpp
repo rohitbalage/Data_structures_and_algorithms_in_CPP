b3943c48d9891a683919e555fd78014565f84dd2
Rohit Balage <thatcomputerboy26@gmail.com>
2026-09-08 20:06:01 -0400

    added stack STL library usage by cpp

---
+++ 14. stacks/4.stackSTL_library.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+int main()
+{
+    stack<string> books;
+    books.push("C++");
+    books.push("Java");
+    books.push("Python");
+    books.push("OS");
+    books.push("DBMS");
+
+    while(!books.empty())
+    {
+        cout << books.top();
+        books.pop();
+    }
+
+    return 0;
+}
